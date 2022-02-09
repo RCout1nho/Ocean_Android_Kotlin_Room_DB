e423fe065f1692a4b343e35a48442c387ed90112
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-02-09 08:59:43 -0400

    first commit

---
+++ app/src/main/java/com/example/ocean_android_kotlin_room_db/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ocean_android_kotlin_room_db
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
